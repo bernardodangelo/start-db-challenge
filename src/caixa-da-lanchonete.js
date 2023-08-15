fe90f15a7f52fb216c617cfcad61445dd223c7bc
bernardo <bernardodangelo8@gmail.com>
2023-08-15 02:51:01 -0300

    feat: add menu

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,9 +1,17 @@
 class CaixaDaLanchonete {
 
     calcularValorDaCompra(metodoDePagamento, itens) {
-        return "";
+        const menu = {
+            cafe: 3.00,
+            chantily: 1.50,
+            suco: 6.20,
+            sanduiche: 6.50,
+            queijo: 2.00,
+            salgado: 7.25,
+            combo1: 9.50,
+            combo2: 7.50,
+        };
     }
-
 }
 
 export { CaixaDaLanchonete };
